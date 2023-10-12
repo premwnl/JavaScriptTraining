f7c99d02ee1be911212ea9228bfbdc674a58df9e
Your Name <premdhonithiru1111@gmail.com>
2023-10-12 10:54:57 +0530

    updated 29.js again.1

--- JAVASCRIPT/Ex-29/ex29.js
+++ JAVASCRIPT/Ex-29/ex29.js
@@ -24,7 +24,15 @@ const DATA = [
     { id: 1, name: 'Vhitech', data: [{ id: 1, name: 'vhitech1' }, { id: 2, name: 'vhitech2' }, { id: 3, name: 'vhitech3' }] },
     { id: 2, name: 'Google', data: [{ id: 1, name: 'google1' }] },
     { id: 3, name: 'Yahoo', },
-    { id: 4, name: 'Duck&Go', data: [{ id: 1, name: 'Duck&Go1', data: [{ id: 1, name: 'Duck&Go_sub', }] }, { id: 2, name: 'Duck&Go2' }, { id: 3, name: 'Duck&Go3' }] },
+    {
+        id: 4, name: 'Duck&Go', data: [
+            {
+                id: 1, name: 'Duck&Go1', data: [
+                    { id: 1, name: 'Duck&Go_sub', }]
+            },
+            { id: 2, name: 'Duck&Go2' },
+            { id: 3, name: 'Duck&Go3' }]
+    },
     { id: 5, name: 'Bing', }
 ];
 
