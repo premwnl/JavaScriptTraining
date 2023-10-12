b977cc114ba4e72d295c04bdd207a5f42c279176
Your Name <premdhonithiru1111@gmail.com>
2023-10-12 10:52:01 +0530

    updated 29.js

--- JAVASCRIPT/Ex-29/ex29.js
+++ JAVASCRIPT/Ex-29/ex29.js
@@ -4,7 +4,7 @@
                                             Developed for              : VHITECH Training Program         *
                                                 Maintenance History                                                    *
                                             Developer                  : Premkumar T                                                      *
-                                            Creation date              : 11/10/2023     Ticket No:               *
+                                            Creation date              : 12/10/2023     Ticket No:               *
 **/
 
 
@@ -21,50 +21,11 @@ const container = document.getElementById('dropDown_container')
 
 //Constant declaration
 const DATA = [
-    {
-        id: 1,
-        name: 'Vhitech',
-        data: [
-            { id: 1, name: 'vhitech1' },
-            { id: 2, name: 'vhitech2' },
-            { id: 3, name: 'vhitech3' }
-        ]
-    },
-    {
-        id: 2,
-        name: 'Google',
-        data: [
-            { id: 1, name: 'google1' }
-        ]
-    },
-    {
-        id: 3, name: 'Yahoo',
-    },
-    {
-        id: 4, name: 'Duck&Go',
-        data: [
-            {
-                id: 1, name: 'Duck&Go1',
-                data: [
-                    {
-                        id: 1, name: 'Duck&Go_sub',
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: 'Duck&Go2'
-            },
-            {
-                id: 3,
-                name: 'Duck&Go3'
-            }
-        ]
-    },
-    {
-        id: 5,
-        name: 'Bing',
-    }
+    { id: 1, name: 'Vhitech', data: [{ id: 1, name: 'vhitech1' }, { id: 2, name: 'vhitech2' }, { id: 3, name: 'vhitech3' }] },
+    { id: 2, name: 'Google', data: [{ id: 1, name: 'google1' }] },
+    { id: 3, name: 'Yahoo', },
+    { id: 4, name: 'Duck&Go', data: [{ id: 1, name: 'Duck&Go1', data: [{ id: 1, name: 'Duck&Go_sub', }] }, { id: 2, name: 'Duck&Go2' }, { id: 3, name: 'Duck&Go3' }] },
+    { id: 5, name: 'Bing', }
 ];
 
 //Error declaration
@@ -80,16 +41,18 @@ function showDropDown(data, id, element) {
     if (element) while (element.nextElementSibling) element.nextElementSibling?.remove(); //Remove if it has next element sibling 
 
     const selectElement = document.createElement('select')
+
     if (id) data = data[id - 1].data ? data[id - 1].data : null;// filtering data by id
 
     if (data) {
+
         for (const index of data) {//creating option and appending to container
             const optionElement = document.createElement('option')
             optionElement.value = index.id
             optionElement.textContent = index.name
             selectElement.append(optionElement)
-
         }
+
         container.append(selectElement)
     }
 
