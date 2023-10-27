c9192947d981969c2a67650f29f6ca219e877b30
Your Name <premdhonithiru1111@gmail.com>
2023-10-27 09:20:34 +0530

    updated 32.2 js oct 27

--- JAVASCRIPT/Ex-32/ex32.2.js
+++ JAVASCRIPT/Ex-32/ex32.2.js
@@ -84,12 +84,14 @@ const createAndUpdate = (card, player) => {//common function for handling cards
     }
 }
 const nextPlayer = (array, player, play) => {//switching turns
+    let item = openCards[0];
     if (saidUNO && playerCards.length <= 0) { playerTurn = false; cpuTurn = true; showResult('player'); }
     else if (cpuCards.length <= 0) { playerTurn = false; cpuTurn = true; showResult('cpu'); }
-    let item = openCards[0];
+
     ((!play.took == play.drop) || cpuTurn) && playerCards.length > 0 ? (playerTurn = !playerTurn, cpuTurn = !cpuTurn) : null;//swapping turn
 
     item.color == 'wild' ? getColor.style.opacity = 100 : getColor.style.opacity = 0;//show color while open card is wild
+
     play.drop && !player && item.color == 'wild' ? (getColor.style.background = `${colors[Math.floor(Math.random() * 4)]}`) : null;//setting random color on wils cards 
     play.drop && (item.value == 'reverse' || item.value == 'skip' || item.value == 'wildDrawFour' || item.value == 'drawTwo') ?
         ((player ? (cpuTurn = false, playerTurn = true) :
@@ -102,7 +104,19 @@ const nextPlayer = (array, player, play) => {//switching turns
                 playerTurn = false; cpuTurn = true;
                 checkTurn(); computerPlay(); pause.style.opacity = 0;
             })) : null);
-    if (array.length == 1) {//uno button
+    const unoButton = () => {
+        let item = openCards[0];
+        playerCards.length > 0 ? (clicked = true, saidUNO = true) : (clicked = false, saidUNO = false);
+        (item.value == 'reverse' || item.value == 'skip' || item.value == 'wildDrawFour' || item.value == 'drawTwo') ?
+            (playerTurn = true, cpuTurn = false, checkTurn()) : (playerTurn = false, cpuTurn = true, computerPlay())
+        unoIconPlayer.style.opacity = 0;
+        if (play.drop && playerCards.length <= 1 && item.color == 'wild') {
+            colorModal.classList.add('showColor');
+            cpuTurn = false; playerTurn = false;
+        }
+        unoIconPlayer.removeEventListener('click', unoButton);
+    }
+    if (play.drop && array.length == 1) {//uno button
         if (player) {
             unoIconPlayer.style.opacity = 100;
             playerTurn = false;
@@ -111,21 +125,18 @@ const nextPlayer = (array, player, play) => {//switching turns
                 !clicked ? (saidUNO = false, addCardsOnSet(0, 2, 1), unoIconPlayer.style.opacity = 0) :
                     (checkTurn(), clicked = false);
             }, 4000);
-            unoIconPlayer.addEventListener('click', () => {
-                playerCards.length > 0 ? (clicked = true, saidUNO = true) : (clicked = false, saidUNO = false);
-                (item.value == 'reverse' || item.value == 'skip' || item.value == 'wildDrawFour' || item.value == 'drawTwo') ?
-                    (playerTurn = true, cpuTurn = false) : (playerTurn = false, cpuTurn = true, computerPlay())
-                unoIconPlayer.style.opacity = 0;
-            })
+            unoIconPlayer.addEventListener('click', unoButton);
         } else { unoIconCpu.style.opacity = 100; }
     } else { !player ? unoIconCpu.style.opacity = 0 : saidUNO = false; }
 
-    if (play.drop && player && item.color == 'wild') {// show choose color
+    if (play.drop && player && item.color == 'wild' && playerCards.length > 1) {// show choose color
         colorModal.classList.add('showColor');
         cpuTurn = false; playerTurn = false;
     }
     play.took || play.drop || playerTurn ? checkTurn() : null;//turn checking css
     (play.took || play.drop) && cpuTurn ? computerPlay() : null;//computer play if its turn testing
+
+
 }
 const setOpenCard = (item, player) => {//setting open card and possible draw card
     openCardElement.innerHTML = '';//emptying the open deck card and setting 
