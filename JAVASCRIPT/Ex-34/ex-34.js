2a4ce650c257fd0258d4e4abc4aeece7ec223427
Your Name <premdhonithiru1111@gmail.com>
2023-11-01 15:30:55 +0530

    added 34

---
+++ JAVASCRIPT/Ex-34/ex-34.js
@@ -0,0 +1,8 @@
+/*            *
+                                            Name of the challenge      : TIC TAC TOE                       *
+                                            Challenge No               : 34                                                          *
+                                            Developed for              : VHITECH Training Program         *
+                                                Maintenance History                                                    *
+                                            Developer                  : Premkumar T                                                      *
+                                            Creation date              : 01/11/2023     Ticket No:               *
+**/
