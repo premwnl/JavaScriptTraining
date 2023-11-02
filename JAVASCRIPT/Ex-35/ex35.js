4639469d44dc43e09412fd1af8c36e5641a89d1a
Your Name <premdhonithiru1111@gmail.com>
2023-11-02 09:55:24 +0530

    added 35.ques

--- JAVASCRIPT/Ex-35/ex35.js
+++ JAVASCRIPT/Ex-35/ex35.js
@@ -1,99 +1,13 @@
 /*            *
-                                            Name of the challenge      : TIC TAC TOE                       *
-                                            Challenge No               : 34                                                          *
+                                            Name of the challenge      : QUIZ                       *
+                                            Challenge No               : 35                                                          *
                                             Developed for              : VHITECH Training Program         *
                                                 Maintenance History                                                    *
                                             Developer                  : Premkumar T                                                      *
-                                            Creation date              : 01/11/2023     Ticket No:               *
+                                            Creation date              : 02/11/2023     Ticket No:               *
 **/
 
 
 //DOM declaration
-const boxes = document.querySelectorAll('.box')
-const container = document.getElementById('container')
-const youScore = document.getElementById('youScore')
-const cpuScore = document.getElementById('cpuScore')
 //Constant declaration
-const x = '<i class="fa-solid fa-xmark fa-9x"></i>'
-const o = '<i class="fa-solid fa-o fa-7x"></i>'
-let youTurn = true
-let cpuTurn = false
-let you_Score = 0
-let cpu_Score = 0
-let ticTacToe = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 //Main functions
-boxes.forEach((box) => { box.addEventListener('click', () => { playerMove(box.id) }) })
-const playerMove = (id) => {
-    let index = id.split('_')[1]
-    ticTacToe[index] = '1'
-    document.getElementById(id).innerHTML = x;
-    document.getElementById(id).style.pointerEvents = 'none'
-    swapTurn()
-}
-const cpuMove = () => {
-    if (ticTacToe.includes(0)) {
-        let index = (ticTacToe.includes(0)) ? getRandomNumber() : null;
-        if (index >= 0) {
-            ticTacToe[index] = '2'
-            document.getElementById(`box_${index}`).innerHTML = o;
-            document.getElementById(`box_${index}`).style.pointerEvents = 'none'
-            swapTurn()
-        } else cpuMove()
-    } else setTimeout(() => { reset() }, 1000)
-}
-const getRandomNumber = () => {
-    let index = Math.floor(Math.random() * 9)
-    if (ticTacToe[index]) getRandomNumber()
-    else return index
-}
-const swapTurn = () => {
-    if (noPossibleWin()) {
-        youTurn = !youTurn;
-        cpuTurn = !cpuTurn;
-        cpuTurn ? (container.style.pointerEvents = 'none', setTimeout(() => cpuMove(), 500)) : container.style.pointerEvents = 'all';
-    } else {
-        container.style.pointerEvents = 'none'
-    }
-}
-const noPossibleWin = () => {
-    if ((ticTacToe[0] == ticTacToe[1] == ticTacToe[2]) && ticTacToe[0] != 0 && ticTacToe[1] != 0 && ticTacToe[2] != 0) {
-        showWinner(ticTacToe[1])
-        return false
-    } else if ((ticTacToe[3] == ticTacToe[4] == ticTacToe[5]) && ticTacToe[3] != 0 && ticTacToe[4] != 0 && ticTacToe[5] != 0) {
-        showWinner(ticTacToe[4])
-        return false
-    } else if ((ticTacToe[6] == ticTacToe[7] == ticTacToe[8]) && ticTacToe[6] != 0 && ticTacToe[7] != 0 && ticTacToe[8] != 0) {
-        showWinner(ticTacToe[7])
-        return false
-    } else if ((ticTacToe[0] == ticTacToe[3] == ticTacToe[6]) && ticTacToe[0] != 0 && ticTacToe[3] != 0 && ticTacToe[6] != 0) {
-        showWinner(ticTacToe[3])
-        return false
-    } else if ((ticTacToe[1] == ticTacToe[4] == ticTacToe[7]) && ticTacToe[1] != 0 && ticTacToe[4] != 0 && ticTacToe[7] != 0) {
-        showWinner(ticTacToe[4])
-        return false
-    } else if ((ticTacToe[2] == ticTacToe[5] == ticTacToe[8]) && ticTacToe[2] != 0 && ticTacToe[5] != 0 && ticTacToe[8] != 0) {
-        showWinner(ticTacToe[5])
-        return false
-    } else if ((ticTacToe[0] == ticTacToe[4] == ticTacToe[8]) && ticTacToe[0] != 0 && ticTacToe[4] != 0 && ticTacToe[8] != 0) {
-        showWinner(ticTacToe[4])
-        return false
-    } else if ((ticTacToe[2] == ticTacToe[4] == ticTacToe[6]) && ticTacToe[2] != 0 && ticTacToe[4] != 0 && ticTacToe[6] != 0) {
-        showWinner(ticTacToe[4])
-        return false
-    }
-    return true
-}
-const showWinner = (val) => {
-    val == '1' ? you_Score++ : val == '2' ? cpu_Score++ : null;
-    val == '1' || val === '2' ? (container.style.pointerEvents = 'none', setTimeout(() => {
-        youScore.textContent = you_Score;
-        cpuScore.textContent = cpu_Score;
-        reset()
-    }, 1000)) : null
-}
-const reset = () => {
-    boxes.forEach((box) => { box.innerHTML = ''; box.style.pointerEvents = 'all' })
-    ticTacToe = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    youTurn = true;
-    cpuTurn = false
-}
---
+++ JAVASCRIPT/Ex-35/questions.js
@@ -0,0 +1,191 @@
+
+[
+    {
+        "question": "What is the capital of France?",
+        "options": ["London", "Berlin", "Paris", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
+        "answer": "Mars"
+    },
+    {
+        "question": "Who wrote the play 'Romeo and Juliet'?",
+        "options": ["Charles Dickens", "William Shakespeare", "Jane Austen", "Leo Tolstoy"],
+        "answer": "William Shakespeare"
+    },
+    {
+        "question": "What is the chemical symbol for water?",
+        "options": ["H2O", "CO2", "O2", "CH4"],
+        "answer": "H2O"
+    },
+    {
+        "question": "Which mammal can fly?",
+        "options": ["Bat", "Elephant", "Lion", "Giraffe"],
+        "answer": "Bat"
+    },
+    {
+        "question": "What is the capital of France?",
+        "options": ["London", "Berlin", "Paris", "Rome"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Mars", "Venus", "Jupiter", "Earth"],
+        "answer": "Mars"
+    },
+    {
+        "question": "Who wrote the play 'Romeo and Juliet'?",
+        "options": ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"],
+        "answer": "William Shakespeare"
+    }, {
+        "question": "What is the capital of France?",
+        "options": ["London", "Berlin", "Paris", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
+        "answer": "Mars"
+    },
+    {
+        "question": "Who wrote 'Romeo and Juliet'?",
+        "options": ["Charles Dickens", "William Shakespeare", "Jane Austen", "F. Scott Fitzgerald"],
+        "answer": "William Shakespeare"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "options": ["Au", "Ag", "Fe", "Cu"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which gas do plants absorb from the atmosphere?",
+        "options": ["Oxygen", "Carbon dioxide", "Nitrogen", "Hydrogen"],
+        "answer": "Carbon dioxide"
+    },
+    {
+        "question": "In which year did the Titanic sink?",
+        "options": ["1912", "1907", "1923", "1931"],
+        "answer": "1912"
+    },
+    {
+        "question": "What is the largest mammal in the world?",
+        "options": ["Elephant", "Blue whale", "Giraffe", "Lion"],
+        "answer": "Blue whale"
+    },
+    {
+        "question": "Which continent is known as the 'Land Down Under'?",
+        "options": ["Africa", "Asia", "Australia", "Europe"],
+        "answer": "Australia"
+    },
+    {
+        "question": "What is the chemical symbol for water?",
+        "options": ["O2", "H2O", "CO2", "NaCl"],
+        "answer": "H2O"
+    },
+    {
+        "question": "Who is the author of 'To Kill a Mockingbird'?",
+        "options": ["J.K. Rowling", "George Orwell", "Harper Lee", "J.R.R. Tolkien"],
+        "answer": "Harper Lee"
+    }, {
+        "question": "What is the capital of India?",
+        "options": ["New Delhi", "Mumbai", "Bangalore", "Kolkata"],
+        "answer": "New Delhi"
+    },
+    {
+        "question": "Which river is known as the 'Ganga' in India?",
+        "options": ["Yamuna", "Brahmaputra", "Indus", "Ganges"],
+        "answer": "Ganges"
+    },
+    {
+        "question": "Which Indian festival is also known as the 'Festival of Lights'?",
+        "options": ["Holi", "Diwali", "Eid", "Navratri"],
+        "answer": "Diwali"
+    },
+    {
+        "question": "Who was the first Prime Minister of India?",
+        "options": ["Jawaharlal Nehru", "Mahatma Gandhi", "Sardar Patel", "Rajendra Prasad"],
+        "answer": "Jawaharlal Nehru"
+    },
+    {
+        "question": "What is the national animal of India?",
+        "options": ["Lion", "Tiger", "Elephant", "Leopard"],
+        "answer": "Tiger"
+    },
+    {
+        "question": "Which mountain range separates India from China?",
+        "options": ["Himalayas", "Western Ghats", "Eastern Ghats", "Vindhya Range"],
+        "answer": "Himalayas"
+    },
+    {
+        "question": "What is the official language of India?",
+        "options": ["Hindi", "English", "Sanskrit", "Tamil"],
+        "answer": "Hindi"
+    },
+    {
+        "question": "Who wrote the Indian national anthem 'Jana Gana Mana'?",
+        "options": ["Rabindranath Tagore", "Mahatma Gandhi", "Subhas Chandra Bose", "Bhagat Singh"],
+        "answer": "Rabindranath Tagore"
+    },
+    {
+        "question": "Which state in India is known as the 'Land of Five Rivers'?",
+        "options": ["Punjab", "Rajasthan", "Kerala", "Gujarat"],
+        "answer": "Punjab"
+    },
+    {
+        "question": "In which year did India gain independence from British rule?",
+        "options": ["1947", "1950", "1935", "1962"],
+        "answer": "1947"
+    }, {
+        "question": "What is the result of 2 + 2?",
+        "options": ["3", "4", "5", "6"],
+        "answer": "4"
+    },
+    {
+        "question": "Simplify 6 × 8.",
+        "options": ["42", "44", "48", "56"],
+        "answer": "48"
+    },
+    {
+        "question": "What is 10 ÷ 2?",
+        "options": ["3", "4", "5", "6"],
+        "answer": "5"
+    },
+    {
+        "question": "Find the square root of 25.",
+        "options": ["3", "4", "5", "6"],
+        "answer": "5"
+    },
+    {
+        "question": "What is 3 squared?",
+        "options": ["6", "9", "12", "15"],
+        "answer": "9"
+    },
+    {
+        "question": "Solve for x: 2x + 5 = 11.",
+        "options": ["2", "3", "4", "5"],
+        "answer": "3"
+    },
+    {
+        "question": "What is the area of a rectangle with length 8 and width 6?",
+        "options": ["32", "40", "48", "56"],
+        "answer": "48"
+    },
+    {
+        "question": "Find the value of π (pi) to 2 decimal places.",
+        "options": ["3.12", "3.14", "3.16", "3.18"],
+        "answer": "3.14"
+    },
+    {
+        "question": "What is the sum of the first 10 natural numbers?",
+        "options": ["45", "50", "55", "60"],
+        "answer": "55"
+    },
+    {
+        "question": "If a = 4 and b = 7, what is a + b?",
+        "options": ["8", "10", "11", "12"],
+        "answer": "11"
+    }
+]
+
