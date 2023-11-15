aa00bd27b31f00a3e2ab0563f565449a80386664
Your Name <premdhonithiru1111@gmail.com>
2023-11-15 19:11:35 +0530

    todo done without review

--- REACT/todo/src/components/Form.jsx
+++ REACT/todo/src/components/Form.jsx
@@ -60,45 +60,47 @@ class Form extends React.Component {
           />
           <button type="submit">ADD</button>
         </form>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Estimation (hours)</th>
-              <th>Description</th>
-              <th>Manage</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.todoList?.map((todo, index) => (
-              <tr
-                key={index}
-                style={{
-                  pointerEvents:
-                    this.state.title ||
-                    this.state.estimation ||
-                    this.state.description
-                      ? "none"
-                      : "all",
-                }}
-              >
-                <td>{todo.title}</td>
-                <td>{todo.estimation}</td>
-                <td>{todo.description}</td>
-                <td>
-                  <i
-                    className="fa-solid fa-edit"
-                    onClick={() => this.handleEdit(index)}
-                  ></i>
-                  <i
-                    className="fa-solid fa-trash"
-                    onClick={() => this.props.handleDelete(index)}
-                  ></i>
-                </td>
+        {this.props.todoList.length && (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Estimation (hours)</th>
+                <th>Description</th>
+                <th>Manage</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.props.todoList?.map((todo, index) => (
+                <tr
+                  key={index}
+                  style={{
+                    pointerEvents:
+                      this.state.title ||
+                      this.state.estimation ||
+                      this.state.description
+                        ? "none"
+                        : "all",
+                  }}
+                >
+                  <td>{todo.title}</td>
+                  <td>{todo.estimation}</td>
+                  <td>{todo.description}</td>
+                  <td>
+                    <i
+                      className="fa-solid fa-edit"
+                      onClick={() => this.handleEdit(index)}
+                    ></i>
+                    <i
+                      className="fa-solid fa-trash"
+                      onClick={() => this.props.handleDelete(index)}
+                    ></i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </>
     );
   }
